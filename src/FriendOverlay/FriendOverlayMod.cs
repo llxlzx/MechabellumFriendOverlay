1cfb2990813349feb0b00c29c6495c5e92b3b1ef
llxlzx <319323959@qq.com>
2026-09-21 20:51:14 +0800

    chore: credit the mod author as llxmod

--- src/FriendOverlay/FriendOverlayMod.cs
+++ src/FriendOverlay/FriendOverlayMod.cs
@@ -7,7 +7,7 @@ using FriendOverlay.UI;
 using MelonLoader;
 using UnityEngine;
 
-[assembly: MelonInfo(typeof(FriendOverlay.FriendOverlayMod), "FriendOverlay", "0.3.40", "MechabellumFriendOverlay")]
+[assembly: MelonInfo(typeof(FriendOverlay.FriendOverlayMod), "FriendOverlay", "0.3.40", "llxmod")]
 [assembly: MelonGame("GameRiver", "Mechabellum")]
 
 namespace FriendOverlay
